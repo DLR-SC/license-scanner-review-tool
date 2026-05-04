bb38e711f7c22477beb682778fdbb7c862ecf347
Lukas Hass <lukas@slucky.de>
2026-05-04 18:27:18 +0200

    add header padding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="leading-none max-h-screen lg:flex lg:items-center border-b">
+  <header class="leading-none max-h-screen lg:flex lg:items-center border-b p-1">
     <h1 class="text-xl font-bold text-gray-900 sm:text-2xl">License Scanner Review Tool</h1>
   </header>
   <RouterView />
